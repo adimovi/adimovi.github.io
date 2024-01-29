f90ef74b14bd949ed081d9f5568f8ecb2bc443a8
adimovi <102922574+adimovi@users.noreply.github.com>
2024-01-30 01:04:14 +0200

    Update navigation.js

--- bboosters/navigation.js
+++ bboosters/navigation.js
@@ -1,23 +1,28 @@
 try {    var menu_bar = document.createRange().createContextualFragment(
             `
-            <div class="container-fluid header-bg py-2">
-            <div class="container d-flex" style="justify-content: space-between;">
-                <a href="https://www.instagram.com/brandboostersdigitalmarketing/" class="more">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
-                    <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"/>
-                  </svg>
-                </a>
-                  <a href="mailto:digitalmarketing.brandboosters@gmail.com" class="more">&#9993; <u>digitalmarketing.brandboosters@gmail.com</u></a>
+            <div class="fixed-top">
+            <div class="container-fluid  header-bg py-2">
+            <div class="container text-center"  >
+                
+                <h3 class="mb-0 text-white">
+                <i class="fa-brands fa-instagram"></i>
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-linkedin"></i>
+                <i class="fa-regular fa-envelope"></i>
+                </h3>
             </div>
          </div>
          <nav class="navbar navbar-expand-lg bg-light">
     <div class="container-fluid">
+    
         <div class="container">
+        
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+            
             <div class="collapse navbar-collapse" id="navbarText">
-                <a class="navbar-brand" href="index.html">
+                <a class="navbar-brand d-none d-md-block" href="index.html">
                     <img src="./statics/img/logo.webp" style="width: 175px;"> 
                 </a>
 
@@ -52,7 +57,9 @@ try {    var menu_bar = document.createRange().createContextualFragment(
     <!-- Bootstrap 5 requires Popper.js and Bootstrap.js for dropdowns -->
     <script src="https://unpkg.com/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://unpkg.com/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/cf94516c3a.js" crossorigin="anonymous"></script>
 </nav>
+</div>
 
 
 `
@@ -97,4 +104,53 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
 
+function numara(maxNr,counterID,speed) {
+  var maxNumber = maxNr;
+  maxNumber = parseInt(maxNumber, 10);
+  if (!isNaN(maxNumber)) {
+  var counterDisplay = document.getElementById(counterID);
+  var count = 0;
+  console.log(counterDisplay);
+  var interval = setInterval(function() {
+    counterDisplay.textContent = numberWithCommas(count);
+    if (count >= maxNumber) {
+        clearInterval(interval);
+    }
+    count=count+1;
+  }, speed);
+  } else {
+  alert("Please enter a valid number.");
+  }
+  };
+
+
+// Your function to be executed
+function laScroll() {
+numara(8,'counter-display1','100');
+numara(100,'counter-display2','15');
+numara(150,'counter-display3','15');
+}
+
+
+
+// Check if the trigger point is scrolled over
+function checkScroll() {
+  var triggerPoint = document.getElementById('trigger-point');
+  var triggerPosition = triggerPoint.getBoundingClientRect().top + window.scrollY;
+  var currentScroll = window.pageYOffset;
+
+  if (currentScroll >= triggerPosition) {
+      // Call your function
+      laScroll();
+      
+      // Optionally, remove the scroll event listener to prevent multiple calls
+      window.removeEventListener('scroll', checkScroll);
+  }
+}
+
+// Attach the scroll event listener
+window.addEventListener('scroll', checkScroll);
