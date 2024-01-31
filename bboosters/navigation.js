d0c67a736df6d13976ec13119a6d532c93bb8ae3
adimovi <102922574+adimovi@users.noreply.github.com>
2024-01-31 12:25:28 +0200

    Update navigation.js

--- bboosters/navigation.js
+++ bboosters/navigation.js
@@ -18,7 +18,7 @@ try {    var menu_bar = document.createRange().createContextualFragment(
    
         <div class="container">
          <div class="row">
-         <div class="col-3">
+         <div class="col-3 align-self-center">
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
